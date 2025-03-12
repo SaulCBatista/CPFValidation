1d712453f8bf759812b3804e8fb517a9a7a81656
SaulCBatista <saulcbatista@gmail.com>
2025-03-11 22:45:54 -0300

    add implementation of the `UDP version` of the client-server system

--- src/com/ufal/aracomp/client/UDP/UDPClient.java
+++ src/com/ufal/aracomp/client/UDP/UDPClient.java
@@ -1,5 +1,37 @@
 package com.ufal.aracomp.client.UDP;
 
-public class UDPClient {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
+public class UDPClient {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        try (Scanner scanner = new Scanner(System.in)) {
+            try {
+                socket = new DatagramSocket();
+                InetAddress serverAddress = InetAddress.getByName("localhost");
+
+                System.out.println("Digite o CPF para validar:");
+                String cpf = scanner.nextLine();
+
+                DatagramPacket sendPacket = new DatagramPacket(cpf.getBytes(), cpf.length(), serverAddress, 12345);
+                socket.send(sendPacket);
+
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Reposta do servidor: " + response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            }
+        }
+    }
 }
--- src/com/ufal/aracomp/server/UDP/UDPServer.java
+++ src/com/ufal/aracomp/server/UDP/UDPServer.java
@@ -1,5 +1,41 @@
 package com.ufal.aracomp.server.UDP;
 
-public class UDPServer {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+import com.ufal.aracomp.util.CPFValidator;
+
+public class UDPServer {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+
+        try {
+            socket = new DatagramSocket(12345);
+            System.out.println("Servidor UDP aguardando...");
+
+            while (true) {
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String cpf = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                boolean isValid = CPFValidator.validade(cpf);
+                String response = isValid ? "CPF válido " : "CPF inválido ";
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                DatagramPacket sendPacket = new DatagramPacket(response.getBytes(), response.length(), clientAddress,
+                        clientPort);
+                socket.send(sendPacket);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null && socket.isClosed()) {
+                socket.close();
+            }
+        }
+    }
 }
